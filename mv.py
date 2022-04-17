f6c3a59dc11eba2330226060af1cffe88e749c3e
Luujamm <luujamm@gmail.com>
2022-04-17 23:28:52 +0800

    0417

--- mv.py
+++ mv.py
@@ -12,27 +12,50 @@ from pypfopt.risk_models import CovarianceShrinkage
 from pypfopt.efficient_frontier import EfficientFrontier
 
 
+DATA = 'data/cb5_2_0410'
+TARGET_NUM = 20
+
+def mv_weights(year, Q):
+    path = './' + DATA + '/' + 'train' + '/' + str(year) + 'Q' + str(Q) + '.csv'
+    df = pd.read_csv(path)
+    df = df[['date', 'close', 'tic']]
+    df = df.pivot(index='date', columns='tic', values='close')
+    mu = mean_historical_return(df)
+    S = CovarianceShrinkage(df).ledoit_wolf()
+    ef = EfficientFrontier(mu, S)
+    weights = ef.max_sharpe()
+    return weights
+
+
+def test(year, Q, test_start_date):
+    target_stocks = get_targets(year=year, Q=Q, num=TARGET_NUM)
+    weights = mv_weights(year, Q)
+    #w = list(weights.itervalues())
+    print(list(weights.values()))
+    #print(w)
+    
+    exit()
+    test_history, test_dating = get_data(target_stocks, year, Q, 'test')
+    test_data = test_history.copy()
+    env = PortfolioEnv(args, test_history, test_data, action_dim, 
+                           test_dating, tu_his, steps=50,
+                           sample_start_date=test_start_date)
 
 
 def main():
-    data_repo = 'data/cb5_2_0410'
     years, quaters = get_years_and_quarters()
-    target_num = 20
-    for year in years:
-        for Q in quaters:
-            target_stocks = get_targets(year=year, Q=Q, num=target_num)
-            train_history, train_dating = get_data(target_stocks, year, Q, 'train')
-            path = './' + data_repo + '/' + 'train' + '/' + str(year) + 'Q' + str(Q) + '.csv'
-            df = pd.read_csv(path)
-            df = df[['date', 'close', 'tic']]
-            df = df.pivot(index='date', columns='tic', values='close')
-            mu = mean_historical_return(df)
-            S = CovarianceShrinkage(df).ledoit_wolf()
-            ef = EfficientFrontier(mu, S)
-            weights = ef.max_sharpe()
-            print(weights)
+    quarter_dates = get_quarter_dates()
 
-            exit()
+    for year in years:
+        for Q in quaters:    
+            key = str(year) + '_Q' + str(Q) 
+            test_start_date = quarter_dates[key][2]
+            test(year, Q, test_start_date)
+
+            
+            
+
+            
 
 if __name__ == '__main__':
     main()
