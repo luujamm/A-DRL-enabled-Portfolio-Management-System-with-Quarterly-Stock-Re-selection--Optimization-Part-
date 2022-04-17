415cd4ad56dd7a5b0f89c8af000bbf044f899d1d
Luujamm <luujamm@gmail.com>
2022-04-17 23:29:00 +0800

    0417

--- mv.py
+++ mv.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 from environment.portfolio_new import PortfolioEnv
+from utils.define_args import define_args
 from utils.data import *
 from utils.yahoodownloader import get_data
 from utils.draw import draw_test_figs, show_test_results, show_val_results
@@ -27,30 +28,34 @@ def mv_weights(year, Q):
     return weights
 
 
-def test(year, Q, test_start_date):
+def test(args, year, Q, test_start_date):
     target_stocks = get_targets(year=year, Q=Q, num=TARGET_NUM)
     weights = mv_weights(year, Q)
-    #w = list(weights.itervalues())
-    print(list(weights.values()))
-    #print(w)
-    
-    exit()
+    weights = list(weights.values())
+    weights.insert(0, 0)
+
     test_history, test_dating = get_data(target_stocks, year, Q, 'test')
+    tu_his, _ = get_data(target_stocks, year, Q, 'tu')
     test_data = test_history.copy()
+    action_dim = TARGET_NUM + 1
     env = PortfolioEnv(args, test_history, test_data, action_dim, 
                            test_dating, tu_his, steps=50,
                            sample_start_date=test_start_date)
+    env.reset()
+    for t in itertools.count(start=1):
+        weights, _, _, _, _, mv_trade_info, _ = env.step(weights, weights)
+        
 
 
 def main():
     years, quaters = get_years_and_quarters()
     quarter_dates = get_quarter_dates()
-
+    args = define_args()
     for year in years:
         for Q in quaters:    
             key = str(year) + '_Q' + str(Q) 
             test_start_date = quarter_dates[key][2]
-            test(year, Q, test_start_date)
+            test(args, year, Q, test_start_date)
 
             
             
