a4e244f8687162aae5d1910c5db28d1763a16654
Luujamm <luujamm@gmail.com>
2022-04-18 18:38:24 +0800

    add_render_only

--- out.py
+++ out.py
@@ -1,7 +1,16 @@
+import sys
 from utils.create_test_file import create_testfile
 from run import main as run
 from utils.render import render
 
-#create_testfile()
-#run()
-render()
+
+def main():
+    args = sys.argv[1:]
+    if len(args) > 0:
+        create_testfile()
+        run()    
+    render()
+
+
+if __name__ == '__main__':
+    main()
