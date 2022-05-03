896cd4e0e2ba5ed7aa463b4efa537da374aef281
Luujamm <luujamm@gmail.com>
2022-05-03 14:48:20 +0800

    0503

--- final.py
+++ final.py
@@ -17,11 +17,15 @@ R = PPO_TCN_PATH
 R1 = 'save_/2022-04-24/120927'
 R0 = 'save_/2022-04-24/121248'
 # ew
+GROUP1 = PPO_TCN_PATH
 GROUP2 = 'data/ew/ew_g2.pickle'
 GROUP3 = 'data/ew/ew_g3.pickle'
 GROUP4 = 'data/ew/ew_g4.pickle'
 GROUP5 = 'data/ew/ew_g5.pickle'
 ALL = 'data/cb5_2_0410/ew_all.pickle'
+# ew 2
+RE = PPO_TCN_PATH
+NO_RE = 'data/ew/ew_no_re.pickle'
 
 def load_values(path):
 
@@ -92,7 +96,7 @@ def load_ew(path):
 
 
 def ew():
-    _, _, group1, _, sp100, dates = load_values(TU_140)
+    _, _, group1, _, sp100, dates = load_values(GROUP1)
     group2 = load_ew(GROUP2)
     group3 = load_ew(GROUP3)
     group4 = load_ew(GROUP4)
@@ -102,10 +106,22 @@ def ew():
     ax = plt.subplot()
     ax.plot(dates, group1, dates, group2, dates, group3, dates, group4, dates, group5, dates, all_, dates, sp100)
     #ax.plot(dates, ew_10, dates, ew_30)
-    legend = ['Group 1', 'Group 2', 'Group 3', 'Group 4', 'Group 5', 'All stocks', 'S&P 100']#, '10', '30']
+    legend = ['Group 1', 'Group 2', 'Group 3', 'Group 4', 'Group 5', 'Full Sample', 'S&P 100']#, '10', '30']
     f = 'ew'
     fig_setting(ax, legend, f)
     print(group1[-1], group2[-1], group3[-1], group4[-1], group5[-1], all_[-1], sp100[-1])
+
+
+def re():
+    _, _, re, _, sp100, dates = load_values(RE)
+    no_re = load_ew(NO_RE)
+    plt.figure(figsize=(8, 6))
+    ax = plt.subplot()
+    ax.plot(dates, re, dates, no_re, dates, sp100)
+    #ax.plot(dates, ew_10, dates, ew_30)
+    legend = ['With Re-selection', 'Without Re-selection', 'S&P 100']
+    f = 're'
+    fig_setting(ax, legend, f)
     
 
 def main():
@@ -118,6 +134,8 @@ def main():
         reward()
     elif len(args) == 1 and args[0] == '--ew':
         ew()
+    elif len(args) == 1 and args[0] == '--re':
+        re()
 
         
 
